0b60b40f8d5980f8ed1decf02585f505771cd2de
strakins <akinolablessing25@gmail.com>
2024-02-08 22:49:46 +0100

    Created Dashboard page and improved the iam of the webapp

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
-      },
+      }, 
       colors: {
         'primary-orange': '#FF5722',
       }
